1e7e5d57603d9842b8f31d465b92a82f9451e007
Patrick Burke <patrick@macbook-pro.mynetworksettings.com>
2024-07-28 12:46:25 -0400

    Index Init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Routes, Route, Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -14,13 +14,13 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/products" component={Products} />
-          <Route path="/careers" component={Careers} />
-          <Route path="/connect" component={Connect} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/connect" element={<Connect />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
